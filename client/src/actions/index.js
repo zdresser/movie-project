c8b6cd76086870c8ff0be6478c2e5791f72f6a34
amhayslip <amhayslip@gmail.com>
2019-01-04 10:00:02 -0500

    remove server things for start

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { FETCH_MOVIES } from './types';
 
-export const fetchMovies = (page = 1) => dispatch => {
+export const fetchMovies = () => dispatch => {
   axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=a50dd974dc6bceb5358b37229983facc&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false`
   ).then(function (response) {
     dispatch({ type: FETCH_MOVIES, payload: response.data });
