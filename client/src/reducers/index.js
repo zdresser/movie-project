3cc12a1a702b984484341622273a7a1ea056847e
amhayslip <amhayslip@gmail.com>
2019-01-06 13:57:50 -0500

    update

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from "redux";
-import { reducer as formReducer } from 'redux-form';
 import MoviesReducer from "./reducer-movies";
 import TotalPagesReducer from "./reducer-total-pages";
 
