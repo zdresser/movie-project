296ab7dd56bdfa193c02079c7f1f5e9f60c38227
amhayslip <amhayslip@gmail.com>
2019-01-09 08:43:21 -0500

    remove page

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { FETCH_MOVIES } from './types';
 
-export const fetchMovies = (page = 1) => dispatch => {
-  axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_MOVIE_DB_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+export const fetchMovies = () => dispatch => {
+  axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_MOVIE_DB_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false`
   ).then(function (response) {
     dispatch({ type: FETCH_MOVIES, payload: response.data });
   })
