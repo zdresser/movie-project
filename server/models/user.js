31bbc1895e4c6bcf453dd79e4b7e8a6af41e6e88
amhayslip <amhayslip@gmail.com>
2021-05-12 11:02:09 -0400

    add server code

---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+// Define our model
+const UserSchema = new Schema({
+  email: { type: String, unique: true, lowercase: true },
+  hash: String,
+  salt: String
+});
+
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+
+  return this.hash === hash;
+};
+
+const UserModel = mongoose.model('user', UserSchema);
+
+module.exports = UserModel;
