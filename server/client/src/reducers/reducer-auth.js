833a299a602c6e44c23155129f2ac1d196c78acb
amhayslip <amhayslip@gmail.com>
2021-05-13 11:35:53 -0400

    add server code for watch list

--- server/client/src/reducers/reducer-auth.js
+++ server/client/src/reducers/reducer-auth.js
@@ -2,14 +2,15 @@ import { AUTH_USER, AUTH_ERROR } from '../actions/types';
 
 const INITIAL_STATE = {
   authenticated: localStorage.getItem('token') || '',
-  errorMessage: ''
+  errorMessage: '',
+  email: null
 };
 
 export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
     case AUTH_USER:
       return { ...state, authenticated: action.payload.token,
-          email: action.payload.email
+          email: action.payload.email || null
         };
     case AUTH_ERROR:
       return { ...state, errorMessage: action.payload };
---
+++ server/controllers/watchList.js
@@ -0,0 +1,16 @@
+const Movie = require('../models/movie');
+const User = require('../models/user');
+
+exports.addMovieToList = function(req, res) {
+  User.findOne({_id: req.user._id}, function (err, user) {
+    const movie = new Movie.MovieModel(req.body.movie);
+    
+    movie.save(function (err, movie) {
+      user.watchList.push(movie);
+
+      user.save(function (err, user) {
+        res.send(movie);
+      });
+    });
+  });
+};
